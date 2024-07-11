bd6a9d9e1fb3921aa4446890d2dfe76e1ff91ea1
Mohammadabukhurj <mohammad.abukhurj2001@gmail.com>
2024-07-12 00:57:14 +0300

    Added input for the user in the program start

--- FCS_54_Mohammad_abu_Khurj_project.py
+++ FCS_54_Mohammad_abu_Khurj_project.py
@@ -1,2 +1,7 @@
 def running_program():
     print("Hello! Please enter: ")
+    print("1. To go to the drivers' menu")
+    print("2. To go to the cities' menu")
+    print("3. To exit the system")
+    user_input = int(input())
+    
