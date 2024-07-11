8f3ef5c18bb3bd864c8fc2b0aa7efae17e384e01
Mohammadabukhurj <mohammad.abukhurj2001@gmail.com>
2024-07-12 01:01:45 +0300

    finished program initialization

--- FCS_54_Mohammad_abu_Khurj_project.py
+++ FCS_54_Mohammad_abu_Khurj_project.py
@@ -3,5 +3,24 @@ def running_program():
     print("1. To go to the drivers' menu")
     print("2. To go to the cities' menu")
     print("3. To exit the system")
+    running= True
     user_input = int(input())
-    
+    while running == True:
+        if user_input == 1:
+            drivers_menu()
+        elif user_input == 2:
+            cities_menu()
+        else:
+            print("Exiting System...")
+            running = False
+
+
+def drivers_menu():
+    pass
+
+
+def cities_menu():
+    pass
+
+
+running_program()
