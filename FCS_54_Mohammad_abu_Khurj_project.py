808178fd620527aa1fae3cb21de2fcf896d1b6bd
Mohammadabukhurj <mohammad.abukhurj2001@gmail.com>
2024-07-12 21:38:32 +0300

    added the view drivers function in the drivers menu

--- FCS_54_Mohammad_abu_Khurj_project.py
+++ FCS_54_Mohammad_abu_Khurj_project.py
@@ -10,17 +10,40 @@ def running_program():
             drivers_menu()
         elif user_input == 2:
             cities_menu()
-        else:
+        elif user_input == 3:
             print("Exiting System...")
             running = False
 
 
 def drivers_menu():
+    print("Hello to the drivers menu! Please enter:")
+    print("1. To view all the drivers")
+    print("2. To add a driver")
+    print("3. To go back to main menu")
+    user_input_drivers = int(input())
+    if user_input_drivers == 1:
+        view_drivers()
+    elif user_input_drivers == 2:
+        add_driver()
+    elif user_input_drivers == 3:
+        return
+
+def view_drivers():
+    for i in drivers_list:
+        print(i+", "+drivers_list[i][0]+", "+drivers_list[i][1])
+
+def add_driver():
     pass
+
+
 
 
 def cities_menu():
     pass
+
+
+
+
 
 
 
