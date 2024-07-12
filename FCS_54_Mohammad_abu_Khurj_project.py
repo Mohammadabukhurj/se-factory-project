d0e968be6289bcfab49ef70fcddb3a4fde697b44
Mohammadabukhurj <mohammad.abukhurj2001@gmail.com>
2024-07-12 21:42:36 +0300

    changed the intros of the running programs to make it so that they run properly

--- FCS_54_Mohammad_abu_Khurj_project.py
+++ FCS_54_Mohammad_abu_Khurj_project.py
@@ -1,11 +1,11 @@
 def running_program():
-    print("Hello! Please enter: ")
-    print("1. To go to the drivers' menu")
-    print("2. To go to the cities' menu")
-    print("3. To exit the system")
     running= True
-    user_input = int(input())
     while running == True:
+        print("Hello! Please enter: ")
+        print("1. To go to the drivers' menu")
+        print("2. To go to the cities' menu")
+        print("3. To exit the system")
+        user_input = int(input())
         if user_input == 1:
             drivers_menu()
         elif user_input == 2:
@@ -16,18 +16,19 @@ def running_program():
 
 
 def drivers_menu():
-    print("Hello to the drivers menu! Please enter:")
-    print("1. To view all the drivers")
-    print("2. To add a driver")
-    print("3. To go back to main menu")
-    user_input_drivers = int(input())
-    if user_input_drivers == 1:
-        for i in drivers_list:
-            print(i+", "+drivers_list[i][0]+", "+drivers_list[i][1])
-    elif user_input_drivers == 2:
-        add_driver()
-    elif user_input_drivers == 3:
-        return
+    while True:
+        if user_input_drivers == 1:
+            print("Hello to the drivers menu! Please enter:")
+            print("1. To view all the drivers")
+            print("2. To add a driver")
+            print("3. To go back to main menu")
+            user_input_drivers = int(input())
+            for i in drivers_list:
+                print(i+", "+drivers_list[i][0]+", "+drivers_list[i][1])
+        elif user_input_drivers == 2:
+            add_driver()
+        elif user_input_drivers == 3:
+            return
 
 
 
