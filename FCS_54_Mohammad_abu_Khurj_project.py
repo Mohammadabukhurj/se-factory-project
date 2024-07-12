95f2832f07f231fa5e15bca940791a6dac02b636
Mohammadabukhurj <mohammad.abukhurj2001@gmail.com>
2024-07-12 21:47:11 +0300

    changed program flow and made messages cleaner

--- FCS_54_Mohammad_abu_Khurj_project.py
+++ FCS_54_Mohammad_abu_Khurj_project.py
@@ -5,7 +5,9 @@ def running_program():
         print("1. To go to the drivers' menu")
         print("2. To go to the cities' menu")
         print("3. To exit the system")
-        user_input = int(input())
+        print("\t")
+        user_input = int(input("Your choice:"))
+        print("\t")
         if user_input == 1:
             drivers_menu()
         elif user_input == 2:
@@ -17,14 +19,20 @@ def running_program():
 
 def drivers_menu():
     while True:
+        print("\t")
+        print("Hello to the drivers menu! Please enter:")
+        print("1. To view all the drivers")
+        print("2. To add a driver")
+        print("3. To go back to main menu")
+        print("\t")
+        user_input_drivers = int(input("Your choice for the drivers menu:"))
+        print("\t")
         if user_input_drivers == 1:
-            print("Hello to the drivers menu! Please enter:")
-            print("1. To view all the drivers")
-            print("2. To add a driver")
-            print("3. To go back to main menu")
-            user_input_drivers = int(input())
+            print("---------------------")
             for i in drivers_list:
                 print(i+", "+drivers_list[i][0]+", "+drivers_list[i][1])
+            print("\t")
+            print("---------------------")    
         elif user_input_drivers == 2:
             add_driver()
         elif user_input_drivers == 3:
